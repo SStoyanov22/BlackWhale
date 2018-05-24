1c2545c76e533d326ecff4a86c29820ee757d49f
DanielJelev <jinjaaar@gmail.com>
2018-05-24 20:07:38 +0300

    Add Notifications jquery noty.js

---
+++ BlackWhale.Web/Content/scripts/notify.module.js
@@ -0,0 +1,26 @@
+﻿var notifyModule = (function () {
+
+    function config(message, status) {
+
+        var type = getType(status);
+
+        $.notify({
+            message: message
+        }, {
+            type: type
+        });
+    }
+
+    function getType(status) {
+        switch (status) {
+            case "Success": return "success"; break;
+            case "Fail": return "danger"; break;
+            case "Warning": return 'warning'; break;
+            default: return "success"; break;
+        }
+    }
+
+    return {
+        config : config
+    }
+}())
