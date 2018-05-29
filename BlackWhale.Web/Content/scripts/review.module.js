6acac84065a1c274f2268170b94cfef9da35406d
DanielJelev <jinjaaar@gmail.com>
2018-05-29 23:24:59 +0300

    Add Templates for reviews and separate Navigation

--- BlackWhale.Web/Content/scripts/review.module.js
+++ BlackWhale.Web/Content/scripts/review.module.js
@@ -2,16 +2,18 @@
 
     function configReviewCreateView() {
 
+        var $selector = '#review-text-area';
+        var $articleTemplate = $("#article-template-container").html();
+
         tinymce.init({
-            selector: '#review-text-area',
+            selector: $selector,
             height: 200,
             theme: 'modern',
             plugins: 'print preview fullpage searchreplace autolink directionality visualblocks visualchars fullscreen image link media template codesample table charmap hr pagebreak nonbreaking anchor toc insertdatetime advlist lists textcolor wordcount imagetools contextmenu colorpicker textpattern help',
             toolbar1: 'formatselect | bold italic strikethrough forecolor backcolor | link | alignleft aligncenter alignright alignjustify  | numlist bullist outdent indent  | removeformat',
             image_advtab: true,
             templates: [
-              { title: 'Test template 1', content: 'Test 1' },
-              { title: 'Test template 2', content: 'Test 2' }
+              { title: 'Article Template', content: $articleTemplate }
             ],
             content_css: [
               '//fonts.googleapis.com/css?family=Lato:300,300i,400,400i',
