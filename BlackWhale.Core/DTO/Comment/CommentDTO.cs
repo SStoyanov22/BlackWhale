58bfb3505f65f0de2c0f2389e948938a8c9e0b35
DanielJelev <jinjaaar@gmail.com>
2018-06-04 23:25:11 +0300

    fix github issue

---
+++ BlackWhale.Core/DTO/Comment/CommentDTO.cs
@@ -0,0 +1,20 @@
+﻿namespace BlackWhale.Core.DTO.Comment
+{
+    using System;
+
+    public class CommentDTO
+    {
+        public int Id { get; set; }
+
+
+        public int ReviewId { get; set; }
+
+        public string Content { get; set; }
+
+        public string Author { get; set; }
+
+        public DateTime CreatedOn { get; set; }
+
+        public bool IsModified { get; set; }
+    }
+}
---
+++ BlackWhale.Core/DTO/Comment/CommentEditDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace BlackWhale.Core.DTO.Comment
+{
+    public class CommentEditDTO
+    {
+        public int Id { get; set; }
+
+        public string Content { get; set; }
+
+    }
+}
